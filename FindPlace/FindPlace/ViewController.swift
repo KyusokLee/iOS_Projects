8e3c80beabd1a12d991f4aa8776f4bd31000a613
KyusokLee <kyusok1@naver.com>
2022-08-01 21:28:05 +0900

    FindPlace_ splash Screen and tabbar make

---
+++ FindPlace/FindPlace/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FindPlace
+//
+//  Created by Kyus'lee on 2022/08/01.
+//
+
+import UIKit
+
+//MARK: FindConvini -> Find Convenience Store App
+// Splash Screen: AppleのHuman Interface Guidelineに従い、Splash Screen(Launch Screen)には、アプリのlogoやimageなどの簡単なものだけを載せることをおすすめする。
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
+
