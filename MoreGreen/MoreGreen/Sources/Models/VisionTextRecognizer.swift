ac44ed364f8dee2adc399c50fbb42589aa85621c
KyusokLee <kyusok1@naver.com>
2023-04-05 23:44:40 +0900

    Develop_VisionTextRecognizerの導入完了

--- MoreGreen/MoreGreen/Sources/Models/VisionTextRecognizer.swift
+++ MoreGreen/MoreGreen/Sources/Models/VisionTextRecognizer.swift
@@ -6,32 +6,50 @@
 //
 
 import Foundation
+import UIKit
 import Vision
 
-// Vision FrameWorkを用いたOCRテキスト認証
 // GCP から Visionへ変更: API Requestのコスト削減, Networkプロセスを省くことが可能
 struct VisionTextRecognizer: VisionTextRecognizerProtocol {
-    func recognize(cgImage: CGImage, completion: @escaping([String]) -> Void) {
+   // MARK: - Vision Frameworkでテキスト認証
+    func recognize(ciImage: CIImage, completion: @escaping (([String], Error?) -> Void)) {
+        // テキスト認証結果を格納するString型配列
         var texts: [String] = []
-        let request = VNRecognizeTextRequest { (request, error) in
-            guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
-            for observation in observations {
-                let candidates = observation.topCandidates(5)
-                for candidate in candidates {
-                    print(candidate.string)
-                }
-                texts.append(candidates.first!.string)
+        var request = VNRecognizeTextRequest()
+        
+        request = VNRecognizeTextRequest { (request, error) in
+            guard let observations = request.results as? [VNRecognizedTextObservation], error == nil else {
+                print("The observations you tried are of unexpected types.")
+                completion([], error)
+                return
             }
-            completion(texts)
+            // 最大のテキスト候補を5つまで
+            let maximumCandidates = 5
+            for observation in observations {
+                guard let candidates = observation.topCandidates(maximumCandidates).first else {
+                    continue
+                }
+                texts.append(candidates.string)
+            }
+            completion(texts, nil)
         }
-
-        request.recognitionLanguages = ["ja-JP"]
-        let handler = VNImageRequestHandler(cgImage: cgImage)
-        try? handler.perform([request])
+        
+        // MARK: - 日本語が正しく表示されなかったエラーはrecognitionLanguagesメソッドで日本語に設定するタイミングが原因
+        // 調べたところ、テキスト認証 -> その後、テキストから日本語（以外の言語も）を検出する順番?のイメージ
+        request.recognitionLanguages = ["ja-JP", "ko-KR", "en-US"]
+        let requestHandler = VNImageRequestHandler(ciImage: ciImage, options: [:])
+        // ここで、imageの処理を進める感じ
+        // 画像に対しての解析リクエストを処理するためのオブジェクト
+        DispatchQueue.main.async {
+            do {
+                try requestHandler.perform([request])
+            } catch let error {
+                print("Error: \(error.localizedDescription)")
+            }
+        }
     }
 }
 
 protocol VisionTextRecognizerProtocol {
-    func send(base64String: String, completion: @escaping ((Data?, Error?) -> Void))
-    func recognize(cgImage: CGImage, completion: @escaping([String]) -> Void)
+    func recognize(ciImage: CIImage, completion: @escaping(([String], Error?) -> Void))
 }
