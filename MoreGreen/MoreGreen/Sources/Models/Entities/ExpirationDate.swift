546eb6a3e581ffe412c5a64f3b8dc369c721c18f
KyusokLee <kyusok1@naver.com>
2023-04-10 21:22:13 +0900

    Fix_ ExpirationDate and Presenter name and logic change

---
+++ MoreGreen/MoreGreen/Sources/Models/Entities/ExpirationDate.swift
@@ -0,0 +1,17 @@
+//
+//  EndPeriodImage.swift
+//  MoreGreen
+//
+//  Created by Kyus'lee on 2022/09/23.
+//
+
+import Foundation
+
+// 賞味期限のtext json parsing
+
+// MARK: - 今後, 商品の名前, 価格, 賞味期限といった３週類のpropertyを持つModelにする予定
+struct ExpirationDate {
+    var itemName: String?
+    var expirationDate: String?
+}
+
