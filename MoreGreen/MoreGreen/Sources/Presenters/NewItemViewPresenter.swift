44b7a5b4625b909cb35b51c1b26c3051b11252d8
KyusokLee <kyusok1@naver.com>
2023-04-12 15:12:25 +0900

    fix data model name and presenter name

--- MoreGreen/MoreGreen/Sources/Presenters/NewItemViewPresenter.swift
+++ MoreGreen/MoreGreen/Sources/Presenters/NewItemViewPresenter.swift
@@ -1,42 +1,8 @@
 //
-//  ItemViewPresenter.swift
+//  NewItemViewPresenter.swift
 //  MoreGreen
 //
-//  Created by Kyus'lee on 2022/09/23.
+//  Created by Kyus'lee on 2023/04/12.
 //
 
 import Foundation
-import UIKit
-
-// インタフェースの定義
-protocol TextRecognizeResultView: AnyObject {
-    func shouldShowTextRecognizeResult(with results: [String])
-    func shouldShowRecognitionFailFeedback()
-}
-
-final class NewItemViewPresenter {
-    private let textRecognizer: VisionTextRecognizerProtocol
-    private weak var view: TextRecognizeResultView?
-    
-    init(textRecognizer: VisionTextRecognizerProtocol, view: TextRecognizeResultView) {
-        self.textRecognizer = textRecognizer
-        // イニシャライザでViewを受け取る
-        self.view = view
-    }
-    
-    //VisionはNetworkを介さずに行う ->理由: 内装のフレームワークであるため、Appleプラットフォームで完結
-    func loadTextResult(from image: CIImage) {
-        textRecognizer.recognize(ciImage: image, completion: { (results, error) in
-            guard error == nil else {
-                return
-            }
-            
-            if results.isEmpty {
-                self.view?.shouldShowRecognitionFailFeedback()
-            } else {
-                self.view?.shouldShowTextRecognizeResult(with: results)
-            }
-            
-        })
-    }
-}
