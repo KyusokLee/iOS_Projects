98d8185337e25a3333053669cf64fce7960b0330
KyusokLee <kyusok1@naver.com>
2023-04-19 16:08:02 +0900

    Fix_ PresenterとModelの修正

--- MoreGreen/MoreGreen/Sources/Models/Entities/ExpirationDate.swift
+++ MoreGreen/MoreGreen/Sources/Models/Entities/ExpirationDate.swift
@@ -9,8 +9,10 @@ import Foundation
 
 // 賞味期限のモデル確立
 // MARK: - 今後, 商品の名前, 価格, 賞味期限といった３週類のpropertyを持つModelにする予定
+// Vision Frameworkを使うので、Parsing処理はいらない
 // そのため、ExpirationDateというモデル名前より、Itemという可読性のいいmodel名に変更する
-struct ExpirationDate: Codable {
+
+struct ExpirationDate {
     var expirationEndDate: String?
 }
 
