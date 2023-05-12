3ebad3f5ef364c2518862a7ff0ad5ed2accf6d40
KyusokLee <kyusok1@naver.com>
2023-05-12 23:20:16 +0900

    [Fix] - UnitTest refactoring

--- MoreGreen/MoreGreenTests/Model/ItemElementsCreatorTests.swift
+++ MoreGreen/MoreGreenTests/Model/ItemElementsCreatorTests.swift
@@ -13,81 +13,81 @@ final class ItemElementsCreatorTests: XCTestCase {
     let creator = ItemElementsCreator()
     
     func testResult1() {
-        let recognizedEndDate = "2023-3-21"
-        let endDate = creator.create(from: recognizedEndDate)
+        let recognizedDate = "2023-3-21"
+        let expirationDate = creator.create(from: recognizedDate)
         
-        XCTAssertEqual(endDate.endDate, "2023-3-21")
+        XCTAssertEqual(expirationDate.expirationEndDate, "2023-3-21")
     }
     
     func testResult2() {
-        let recognizedEndDate = "23-03-21"
-        let endDate = creator.create(from: recognizedEndDate)
+        let recognizedDate = "23-03-21"
+        let expirationDate = creator.create(from: recognizedDate)
         
-        XCTAssertEqual(endDate.endDate, "23-03-21")
+        XCTAssertEqual(expirationDate.expirationEndDate, "23-03-21")
     }
     
     func testResult3() {
-        let recognizedEndDate = "23-3-1"
-        let endDate = creator.create(from: recognizedEndDate)
+        let recognizedDate = "23-3-1"
+        let expirationDate = creator.create(from: recognizedDate)
         
-        XCTAssertEqual(endDate.endDate, "23-3-1")
+        XCTAssertEqual(expirationDate.expirationEndDate, "23-3-1")
     }
     
     func testResult4() {
-        let recognizedEndDate = "2023/3/21"
-        let endDate = creator.create(from: recognizedEndDate)
+        let recognizedDate = "2023/3/21"
+        let expirationDate = creator.create(from: recognizedDate)
         
-        XCTAssertEqual(endDate.endDate, "2023/3/21")
+        XCTAssertEqual(expirationDate.expirationEndDate, "2023/3/21")
     }
     
     func testResult5() {
-        let recognizedEndDate = "2023/3/1"
-        let endDate = creator.create(from: recognizedEndDate)
+        let recognizedDate = "2023/3/1"
+        let expirationDate = creator.create(from: recognizedDate)
         
-        XCTAssertEqual(endDate.endDate, "2023/3/1")
+        XCTAssertEqual(expirationDate.expirationEndDate, "2023/3/1")
     }
     
     func testResult6() {
-        let recognizedEndDate = "23/3/1"
-        let endDate = creator.create(from: recognizedEndDate)
+        let recognizedDate = "23/3/1"
+        let expirationDate = creator.create(from: recognizedDate)
         
-        XCTAssertEqual(endDate.endDate, "23/3/1")
+        XCTAssertEqual(expirationDate.expirationEndDate, "23/3/1")
     }
     
     func testResult7() {
-        let recognizedEndDate = "23/03/01"
-        let endDate = creator.create(from: recognizedEndDate)
+        let recognizedDate = "23/03/01"
+        let expirationDate = creator.create(from: recognizedDate)
         
-        XCTAssertEqual(endDate.endDate, "23/03/01")
+        XCTAssertEqual(expirationDate.expirationEndDate, "23/03/01")
     }
     
     func testResult8() {
-        let recognizedEndDate = "2023.03.01"
-        let endDate = creator.create(from: recognizedEndDate)
+        let recognizedDate = "2023.03.01"
+        let expirationDate = creator.create(from: recognizedDate)
         
-        XCTAssertEqual(endDate.endDate, "2023.03.01")
+        XCTAssertEqual(expirationDate.expirationEndDate, "2023.03.01")
     }
     
 //    // 数字と年月日が混ざっている文字列は認識されないことを確認した
 //    func testResult9() {
-//        let recognizedEndDate = "2023年03月01日"
-//        let endDate = creator.create(from: recognizedEndDate)
+//        let recognizedDate = "2023年03月01日"
+//        let expirationDate = creator.create(from: recognizedDate)
 //
-//        XCTAssertEqual(endDate.endDate, "2023年03月01日")
+//        XCTAssertEqual(expirationDate.expirationDate, "2023年03月01日")
 //    }
     
     // 以下賞味期限と日本語、数字が混ざっている時
     func testResult10() {
-        let recognizedEndDate = "2023/3/21\n賞味期限"
-        let endDate = creator.create(from: recognizedEndDate)
+        let recognizedDate = "2023/3/21\n賞味期限"
+        let expirationDate = creator.create(from: recognizedDate)
         
-        XCTAssertEqual(endDate.endDate, "2023/3/21")
+        XCTAssertEqual(expirationDate.expirationEndDate, "2023/3/21")
     }
     
     func testResult11() {
-        let recognizedEndDate = "22212\n2023/3/21\n賞味期限"
-        let endDate = creator.create(from: recognizedEndDate)
+        let recognizedDate = "22212\n2023/3/21\n賞味期限"
+        let expirationDate = creator.create(from: recognizedDate)
         
-        XCTAssertEqual(endDate.endDate, "2023/3/21")
+        XCTAssertEqual(expirationDate.expirationEndDate, "2023/3/21")
     }
 }
