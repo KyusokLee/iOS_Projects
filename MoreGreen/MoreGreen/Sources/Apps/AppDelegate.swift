eff2bdad476a5607a62c89f68b055483c99d7b77
KyusokLee <kyusok1@naver.com>
2024-01-12 18:01:01 +0900

    [Change] - change the ocr model and delete unneccessary sources

--- MoreGreen/MoreGreen/Sources/Apps/AppDelegate.swift
+++ MoreGreen/MoreGreen/Sources/Apps/AppDelegate.swift
@@ -51,6 +51,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
+        
         let container = NSPersistentContainer(name: "MoreGreen")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
